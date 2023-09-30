dc8460925e3247f4fb527f2eaa2f08f613fb777c
G3n4r00 <rm551986@fiap.com.br>
2023-09-30 18:22:20 -0300

    formulario editar criado

--- src/components/ModalActions.jsx
+++ src/components/ModalActions.jsx
@@ -5,8 +5,36 @@ export default function ModalActions(props){
     if(props.open){
         return(
             <div className={styles.container}>
-                <h1>MODAL DE PRODUTOS</h1>
+                <h1>PRODUTOS</h1>
                 <button onClick={()=> props.setOpen(false)}>CLOSE-MODAL</button>
+
+                <div>
+                    <form>
+                        <fieldset>
+                            <legend>Dados do Produto</legend>
+                            <div>
+                                <label htmlFor="idNome">Nome</label>
+                                <input type="text" name="nome" id="idNome"/>
+
+                                <label htmlFor="idDesc">Descrição</label>
+                                <input type="text" name="desc" id="iddesc"/>
+
+                                <label htmlFor="idPreco">Preço</label>
+                                <input type="number" name="preco" id="idpreco"/>
+                            </div>
+                            
+                            <div>
+                                <button>EDITAR</button>
+                            </div>
+
+                        </fieldset>
+                    </form>
+                </div>
+
+
+
+
+
             </div>
         )
     }
